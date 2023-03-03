0af1e06e75b05570d67be4191e252489c2e1b3cb
Dim-ulik <d.martyshev@yandex.ru>
2023-03-04 05:03:50 +0700

    clear

--- MAMP/htdocs/moodle311/blocks/equation/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$settings->add(new admin_setting_heading('block_equation_heading',
-    get_string('settings_heading', 'block_equation'),
-    get_string('settings_content', 'block_equation')));
-
-$settings->add(new admin_setting_configtext('block_equation/Label',
-    get_string('label', 'block_equation'),
-    get_string('label_desc', 'block_equation'), '', PARAM_TEXT));
--- MAMP/htdocs/moodle311/blocks/equation/version.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || exit();
-
-$plugin->version = 2023030110;
-$plugin->requires = 2021051700;
-$plugin->component = 'block_equation';
